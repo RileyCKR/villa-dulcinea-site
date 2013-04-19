51da61852e08ab5b74282417ce52df0a62f31397
Wydra <RileyCKR@gmail.com>
2013-04-19 12:42:15 -0400

    adding photo gallery

---
+++ photo-gallery.php
@@ -0,0 +1,82 @@
+<?php 
+$title = 'Photo Gallery';
+$menuIndex = 4;
+$headerImage = '/images/header/living-room.jpg';
+
+require '/layout/header.php'; ?>
+
+<h2>Exterior</h2>
+<div>Exterior Photos.</div>
+<div class="teaserImage">
+    <table style="border-collapse:collapse;" align="Center" border="0" cellspacing="0">
+	<tbody><tr>
+		<td>
+            <a href="/photo-gallery/exterior/01.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/01.jpg" title=""><img src="/photo-gallery/exterior/01-thumb.jpg" alt="01"></a></td><td>
+            <a href="/photo-gallery/exterior/02.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/02.jpg" title=""><img src="/photo-gallery/exterior/02-thumb.jpg" alt="02"></a></td><td>
+            <a href="/photo-gallery/exterior/03.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/03.jpg" title=""><img src="/photo-gallery/exterior/03-thumb.jpg" alt="03"></a></td><td>
+            <a href="/photo-gallery/exterior/04.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/04.jpg" title=""><img src="/photo-gallery/exterior/04-thumb.jpg" alt="04"></a></td><td>
+            <a href="/photo-gallery/exterior/05.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/05.jpg" title=""><img src="/photo-gallery/exterior/05-thumb.jpg" alt="05"></a>
+        </td>
+	</tr><tr>
+		<td>
+            <a href="/photo-gallery/exterior/06.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/06.jpg" title=""><img src="/photo-gallery/exterior/06-thumb.jpg" alt="06"></a></td><td>
+            <a href="/photo-gallery/exterior/07.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/07.jpg" title=""><img src="/photo-gallery/exterior/07-thumb.jpg" alt="07"></a></td><td>
+            <a href="/photo-gallery/exterior/08.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/08.jpg" title=""><img src="/photo-gallery/exterior/08-thumb.jpg" alt="08"></a></td><td>
+            <a href="/photo-gallery/exterior/09.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/09.jpg" title=""><img src="/photo-gallery/exterior/09-thumb.jpg" alt="09"></a></td><td>
+            <a href="/photo-gallery/exterior/10.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/10.jpg" title=""><img src="/photo-gallery/exterior/10-thumb.jpg" alt="10"></a></td>
+	</tr><tr>
+		<td>
+            <a href="/photo-gallery/exterior/11.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/11.jpg" title=""><img src="/photo-gallery/exterior/11-thumb.jpg" alt="11"></a></td><td>
+            <a href="/photo-gallery/exterior/12.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/12.jpg" title=""><img src="/photo-gallery/exterior/12-thumb.jpg" alt="12"></a></td><td>
+            <a href="/photo-gallery/exterior/13.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/13.jpg" title=""><img src="/photo-gallery/exterior/13-thumb.jpg" alt="13"></a></td><td>
+            <a href="/photo-gallery/exterior/14.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/14.jpg" title=""><img src="/photo-gallery/exterior/14-thumb.jpg" alt="14"></a></td><td></td>
+	</tr></tbody>
+    </table>
+</div>
+        
+<h2>Interior</h2>
+<div>Interior Shots of the Villa.</div>
+<div class="teaserImage">
+    <table style="border-collapse:collapse;" align="Center" border="0" cellspacing="0">
+	<tbody><tr>
+		<td>
+            <a href="/photo-gallery/interior/01.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/01.jpg" title=""><img src="/photo-gallery/interior/01-thumb.jpg" alt="01"></a></td><td>
+            <a href="/photo-gallery/interior/02.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/02.jpg" title=""><img src="/photo-gallery/interior/02-thumb.jpg" alt="02"></a></td><td>
+            <a href="/photo-gallery/interior/03.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/03.jpg" title=""><img src="/photo-gallery/interior/03-thumb.jpg" alt="03"></a></td><td>
+            <a href="/photo-gallery/interior/04.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/04.jpg" title=""><img src="/photo-gallery/interior/04-thumb.jpg" alt="04"></a></td><td>
+            <a href="/photo-gallery/interior/05.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/05.jpg" title=""><img src="/photo-gallery/interior/05-thumb.jpg" alt="05"></a></td>
+	</tr><tr>
+		<td>
+            <a href="/photo-gallery/interior/06.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/06.jpg" title=""><img src="/photo-gallery/interior/06-thumb.jpg" alt="06"></a></td><td>
+            <a href="/photo-gallery/interior/07.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/07.jpg" title=""><img src="/photo-gallery/interior/07-thumb.jpg" alt="07"></a></td><td>
+            <a href="/photo-gallery/interior/08.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/08.jpg" title=""><img src="/photo-gallery/interior/08-thumb.jpg" alt="08"></a></td><td>
+            <a href="/photo-gallery/interior/09.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/09.jpg" title=""><img src="/photo-gallery/interior/09-thumb.jpg" alt="09"></a></td><td>
+            <a href="/photo-gallery/interior/10.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/10.jpg" title=""><img src="/photo-gallery/interior/10-thumb.jpg" alt="10"></a></td>
+	</tr><tr>
+		<td>
+            <a href="/photo-gallery/interior/11.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/11.jpg" title=""><img src="/photo-gallery/interior/11-thumb.jpg" alt="11"></a></td><td>
+            <a href="/photo-gallery/interior/12.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/12.jpg" title=""><img src="/photo-gallery/interior/12-thumb.jpg" alt="12"></a></td><td>
+            <a href="/photo-gallery/interior/13.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/13.jpg" title=""><img src="/photo-gallery/interior/13-thumb.jpg" alt="13"></a></td><td>
+            <a href="/photo-gallery/interior/14.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/14.jpg" title=""><img src="/photo-gallery/interior/14-thumb.jpg" alt="14"></a></td><td>
+            <a href="/photo-gallery/interior/15.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/15.jpg" title=""><img src="/photo-gallery/interior/15-thumb.jpg" alt="15"></a></td>
+	</tr><tr>
+		<td>
+            <a href="/photo-gallery/interior/16.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/16.jpg" title=""><img src="/photo-gallery/interior/16-thumb.jpg" alt="16"></a></td><td>
+            <a href="/photo-gallery/interior/17.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/17.jpg" title=""><img src="/photo-gallery/interior/17-thumb.jpg" alt="17"></a></td><td></td><td></td><td></td>
+	</tr>
+</tbody></table>
+</div>
+        
+<h2>Guest House</h2>
+<div><br></div>
+<div class="teaserImage">
+    <table style="border-collapse:collapse;" align="Center" border="0" cellspacing="0">
+	<tbody><tr>
+		<td>
+            <a href="/photo-gallery/guest-house/01.jpg" rel="lightbox[group]" rev="/photo-gallery/guest-house/01.jpg" title=""><img src="/photo-gallery/guest-house/01-thumb.jpg" alt="01"></a></td><td>
+            <a href="/photo-gallery/guest-house/02.jpg" rel="lightbox[group]" rev="/photo-gallery/guest-house/02.jpg" title=""><img src="/photo-gallery/guest-house/02-thumb.jpg" alt="02"></a></td><td></td><td></td><td></td>
+	</tr>
+    </tbody></table>
+</div>
+
+<?php require '/layout/footer.php'; ?>
