af290eece9b49370481d1a442899f38ae377b19c
Wydra <RileyCKR@gmail.com>
2013-04-19 14:13:09 -0400

    rename photo-gallery to make apache happy

--- photo-gallery.php
+++ photo-gallery.php
@@ -11,25 +11,25 @@ require './layout/header.php'; ?>
     <table style="border-collapse:collapse;" align="Center" border="0" cellspacing="0">
 	<tbody><tr>
 		<td>
-            <a href="/photo-gallery/exterior/01.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/01.jpg" title=""><img src="/photo-gallery/exterior/01-thumb.jpg" alt="01" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/exterior/02.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/02.jpg" title=""><img src="/photo-gallery/exterior/02-thumb.jpg" alt="02" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/exterior/03.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/03.jpg" title=""><img src="/photo-gallery/exterior/03-thumb.jpg" alt="03" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/exterior/04.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/04.jpg" title=""><img src="/photo-gallery/exterior/04-thumb.jpg" alt="04" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/exterior/05.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/05.jpg" title=""><img src="/photo-gallery/exterior/05-thumb.jpg" alt="05" class="thumbWide"></a>
+            <a href="/photo/exterior/01.jpg" rel="lightbox[group]" rev="/photo/exterior/01.jpg" title=""><img src="/photo/exterior/01-thumb.jpg" alt="01" class="thumbWide"></a></td><td>
+            <a href="/photo/exterior/02.jpg" rel="lightbox[group]" rev="/photo/exterior/02.jpg" title=""><img src="/photo/exterior/02-thumb.jpg" alt="02" class="thumbWide"></a></td><td>
+            <a href="/photo/exterior/03.jpg" rel="lightbox[group]" rev="/photo/exterior/03.jpg" title=""><img src="/photo/exterior/03-thumb.jpg" alt="03" class="thumbWide"></a></td><td>
+            <a href="/photo/exterior/04.jpg" rel="lightbox[group]" rev="/photo/exterior/04.jpg" title=""><img src="/photo/exterior/04-thumb.jpg" alt="04" class="thumbWide"></a></td><td>
+            <a href="/photo/exterior/05.jpg" rel="lightbox[group]" rev="/photo/exterior/05.jpg" title=""><img src="/photo/exterior/05-thumb.jpg" alt="05" class="thumbWide"></a>
         </td>
 	</tr><tr>
 		<td>
-            <a href="/photo-gallery/exterior/06.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/06.jpg" title=""><img src="/photo-gallery/exterior/06-thumb.jpg" alt="06" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/exterior/07.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/07.jpg" title=""><img src="/photo-gallery/exterior/07-thumb.jpg" alt="07" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/exterior/08.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/08.jpg" title=""><img src="/photo-gallery/exterior/08-thumb.jpg" alt="08" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/exterior/09.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/09.jpg" title=""><img src="/photo-gallery/exterior/09-thumb.jpg" alt="09" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/exterior/10.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/10.jpg" title=""><img src="/photo-gallery/exterior/10-thumb.jpg" alt="10" class="thumbTall"></a></td>
+            <a href="/photo/exterior/06.jpg" rel="lightbox[group]" rev="/photo/exterior/06.jpg" title=""><img src="/photo/exterior/06-thumb.jpg" alt="06" class="thumbTall"></a></td><td>
+            <a href="/photo/exterior/07.jpg" rel="lightbox[group]" rev="/photo/exterior/07.jpg" title=""><img src="/photo/exterior/07-thumb.jpg" alt="07" class="thumbTall"></a></td><td>
+            <a href="/photo/exterior/08.jpg" rel="lightbox[group]" rev="/photo/exterior/08.jpg" title=""><img src="/photo/exterior/08-thumb.jpg" alt="08" class="thumbTall"></a></td><td>
+            <a href="/photo/exterior/09.jpg" rel="lightbox[group]" rev="/photo/exterior/09.jpg" title=""><img src="/photo/exterior/09-thumb.jpg" alt="09" class="thumbTall"></a></td><td>
+            <a href="/photo/exterior/10.jpg" rel="lightbox[group]" rev="/photo/exterior/10.jpg" title=""><img src="/photo/exterior/10-thumb.jpg" alt="10" class="thumbTall"></a></td>
 	</tr><tr>
 		<td>
-            <a href="/photo-gallery/exterior/11.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/11.jpg" title=""><img src="/photo-gallery/exterior/11-thumb.jpg" alt="11" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/exterior/12.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/12.jpg" title=""><img src="/photo-gallery/exterior/12-thumb.jpg" alt="12" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/exterior/13.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/13.jpg" title=""><img src="/photo-gallery/exterior/13-thumb.jpg" alt="13" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/exterior/14.jpg" rel="lightbox[group]" rev="/photo-gallery/exterior/14.jpg" title=""><img src="/photo-gallery/exterior/14-thumb.jpg" alt="14" class="thumbWide"></a></td><td></td>
+            <a href="/photo/exterior/11.jpg" rel="lightbox[group]" rev="/photo/exterior/11.jpg" title=""><img src="/photo/exterior/11-thumb.jpg" alt="11" class="thumbTall"></a></td><td>
+            <a href="/photo/exterior/12.jpg" rel="lightbox[group]" rev="/photo/exterior/12.jpg" title=""><img src="/photo/exterior/12-thumb.jpg" alt="12" class="thumbWide"></a></td><td>
+            <a href="/photo/exterior/13.jpg" rel="lightbox[group]" rev="/photo/exterior/13.jpg" title=""><img src="/photo/exterior/13-thumb.jpg" alt="13" class="thumbWide"></a></td><td>
+            <a href="/photo/exterior/14.jpg" rel="lightbox[group]" rev="/photo/exterior/14.jpg" title=""><img src="/photo/exterior/14-thumb.jpg" alt="14" class="thumbWide"></a></td><td></td>
 	</tr></tbody>
     </table>
 </div>
@@ -40,29 +40,29 @@ require './layout/header.php'; ?>
     <table style="border-collapse:collapse;" align="Center" border="0" cellspacing="0">
 	<tbody><tr>
 		<td>
-            <a href="/photo-gallery/interior/01.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/01.jpg" title=""><img src="/photo-gallery/interior/01-thumb.jpg" alt="01" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/interior/02.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/02.jpg" title=""><img src="/photo-gallery/interior/02-thumb.jpg" alt="02" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/interior/03.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/03.jpg" title=""><img src="/photo-gallery/interior/03-thumb.jpg" alt="03" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/interior/04.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/04.jpg" title=""><img src="/photo-gallery/interior/04-thumb.jpg" alt="04" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/interior/05.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/05.jpg" title=""><img src="/photo-gallery/interior/05-thumb.jpg" alt="05" class="thumbWide"></a></td>
+            <a href="/photo/interior/01.jpg" rel="lightbox[group]" rev="/photo/interior/01.jpg" title=""><img src="/photo/interior/01-thumb.jpg" alt="01" class="thumbWide"></a></td><td>
+            <a href="/photo/interior/02.jpg" rel="lightbox[group]" rev="/photo/interior/02.jpg" title=""><img src="/photo/interior/02-thumb.jpg" alt="02" class="thumbTall"></a></td><td>
+            <a href="/photo/interior/03.jpg" rel="lightbox[group]" rev="/photo/interior/03.jpg" title=""><img src="/photo/interior/03-thumb.jpg" alt="03" class="thumbWide"></a></td><td>
+            <a href="/photo/interior/04.jpg" rel="lightbox[group]" rev="/photo/interior/04.jpg" title=""><img src="/photo/interior/04-thumb.jpg" alt="04" class="thumbWide"></a></td><td>
+            <a href="/photo/interior/05.jpg" rel="lightbox[group]" rev="/photo/interior/05.jpg" title=""><img src="/photo/interior/05-thumb.jpg" alt="05" class="thumbWide"></a></td>
 	</tr><tr>
 		<td>
-            <a href="/photo-gallery/interior/06.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/06.jpg" title=""><img src="/photo-gallery/interior/06-thumb.jpg" alt="06" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/interior/07.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/07.jpg" title=""><img src="/photo-gallery/interior/07-thumb.jpg" alt="07" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/interior/08.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/08.jpg" title=""><img src="/photo-gallery/interior/08-thumb.jpg" alt="08" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/interior/09.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/09.jpg" title=""><img src="/photo-gallery/interior/09-thumb.jpg" alt="09" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/interior/10.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/10.jpg" title=""><img src="/photo-gallery/interior/10-thumb.jpg" alt="10" class="thumbWide"></a></td>
+            <a href="/photo/interior/06.jpg" rel="lightbox[group]" rev="/photo/interior/06.jpg" title=""><img src="/photo/interior/06-thumb.jpg" alt="06" class="thumbTall"></a></td><td>
+            <a href="/photo/interior/07.jpg" rel="lightbox[group]" rev="/photo/interior/07.jpg" title=""><img src="/photo/interior/07-thumb.jpg" alt="07" class="thumbTall"></a></td><td>
+            <a href="/photo/interior/08.jpg" rel="lightbox[group]" rev="/photo/interior/08.jpg" title=""><img src="/photo/interior/08-thumb.jpg" alt="08" class="thumbTall"></a></td><td>
+            <a href="/photo/interior/09.jpg" rel="lightbox[group]" rev="/photo/interior/09.jpg" title=""><img src="/photo/interior/09-thumb.jpg" alt="09" class="thumbTall"></a></td><td>
+            <a href="/photo/interior/10.jpg" rel="lightbox[group]" rev="/photo/interior/10.jpg" title=""><img src="/photo/interior/10-thumb.jpg" alt="10" class="thumbWide"></a></td>
 	</tr><tr>
 		<td>
-            <a href="/photo-gallery/interior/11.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/11.jpg" title=""><img src="/photo-gallery/interior/11-thumb.jpg" alt="11" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/interior/12.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/12.jpg" title=""><img src="/photo-gallery/interior/12-thumb.jpg" alt="12" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/interior/13.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/13.jpg" title=""><img src="/photo-gallery/interior/13-thumb.jpg" alt="13" class="thumbTall"></a></td><td>
-            <a href="/photo-gallery/interior/14.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/14.jpg" title=""><img src="/photo-gallery/interior/14-thumb.jpg" alt="14" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/interior/15.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/15.jpg" title=""><img src="/photo-gallery/interior/15-thumb.jpg" alt="15" class="thumbTall"></a></td>
+            <a href="/photo/interior/11.jpg" rel="lightbox[group]" rev="/photo/interior/11.jpg" title=""><img src="/photo/interior/11-thumb.jpg" alt="11" class="thumbTall"></a></td><td>
+            <a href="/photo/interior/12.jpg" rel="lightbox[group]" rev="/photo/interior/12.jpg" title=""><img src="/photo/interior/12-thumb.jpg" alt="12" class="thumbTall"></a></td><td>
+            <a href="/photo/interior/13.jpg" rel="lightbox[group]" rev="/photo/interior/13.jpg" title=""><img src="/photo/interior/13-thumb.jpg" alt="13" class="thumbTall"></a></td><td>
+            <a href="/photo/interior/14.jpg" rel="lightbox[group]" rev="/photo/interior/14.jpg" title=""><img src="/photo/interior/14-thumb.jpg" alt="14" class="thumbWide"></a></td><td>
+            <a href="/photo/interior/15.jpg" rel="lightbox[group]" rev="/photo/interior/15.jpg" title=""><img src="/photo/interior/15-thumb.jpg" alt="15" class="thumbTall"></a></td>
 	</tr><tr>
 		<td>
-            <a href="/photo-gallery/interior/16.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/16.jpg" title=""><img src="/photo-gallery/interior/16-thumb.jpg" alt="16" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/interior/17.jpg" rel="lightbox[group]" rev="/photo-gallery/interior/17.jpg" title=""><img src="/photo-gallery/interior/17-thumb.jpg" alt="17" class="thumbTall"></a></td><td></td><td></td><td></td>
+            <a href="/photo/interior/16.jpg" rel="lightbox[group]" rev="/photo/interior/16.jpg" title=""><img src="/photo/interior/16-thumb.jpg" alt="16" class="thumbWide"></a></td><td>
+            <a href="/photo/interior/17.jpg" rel="lightbox[group]" rev="/photo/interior/17.jpg" title=""><img src="/photo/interior/17-thumb.jpg" alt="17" class="thumbTall"></a></td><td></td><td></td><td></td>
 	</tr>
 </tbody></table>
 </div>
@@ -73,8 +73,8 @@ require './layout/header.php'; ?>
     <table style="border-collapse:collapse;" align="Center" border="0" cellspacing="0">
 	<tbody><tr>
 		<td>
-            <a href="/photo-gallery/guest-house/01.jpg" rel="lightbox[group]" rev="/photo-gallery/guest-house/01.jpg" title=""><img src="/photo-gallery/guest-house/01-thumb.jpg" alt="01" class="thumbWide"></a></td><td>
-            <a href="/photo-gallery/guest-house/02.jpg" rel="lightbox[group]" rev="/photo-gallery/guest-house/02.jpg" title=""><img src="/photo-gallery/guest-house/02-thumb.jpg" alt="02" class="thumbTall"></a></td><td></td><td></td><td></td>
+            <a href="/photo/guest-house/01.jpg" rel="lightbox[group]" rev="/photo/guest-house/01.jpg" title=""><img src="/photo/guest-house/01-thumb.jpg" alt="01" class="thumbWide"></a></td><td>
+            <a href="/photo/guest-house/02.jpg" rel="lightbox[group]" rev="/photo/guest-house/02.jpg" title=""><img src="/photo/guest-house/02-thumb.jpg" alt="02" class="thumbTall"></a></td><td></td><td></td><td></td>
 	</tr>
     </tbody></table>
 </div>
