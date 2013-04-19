5a44f812979f84ab2ad11ecec9290cd36ea62e7a
Wydra <RileyCKR@gmail.com>
2013-04-18 22:41:50 -0400

    change links to php

--- layout/menu.php
+++ layout/menu.php
@@ -1,8 +1,8 @@
 <ul id="ListMenu1" class="CMSListMenuUL">
-    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 1) echo 'Highlighted' ?>" href="/Home.aspx">&gt; Home</a></li>
-    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 2) echo 'Highlighted' ?>" href="/Villa-Dulcinea.aspx">Villa Dulcinea</a></li>
-    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 3) echo 'Highlighted' ?>" href="/Area.aspx">&gt; Area</a></li>
-    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 4) echo 'Highlighted' ?>" href="/Photo-Gallery.aspx">&gt; Photo Gallery</a></li>
-    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 5) echo 'Highlighted' ?>" href="/Testimonials.aspx">&gt; Testimonials</a></li>
-    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 6) echo 'Highlighted' ?>" href="/Rent-Now.aspx">&gt; Rent Now</a></li>
+    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 1) echo 'Highlighted' ?>" href="/index.php">&gt; Home</a></li>
+    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 2) echo 'Highlighted' ?>" href="/villa-dulcinea.php">Villa Dulcinea</a></li>
+    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 3) echo 'Highlighted' ?>" href="/area.php">&gt; Area</a></li>
+    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 4) echo 'Highlighted' ?>" href="/photo-gallery.php">&gt; Photo Gallery</a></li>
+    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 5) echo 'Highlighted' ?>" href="/testimonials.php">&gt; Testimonials</a></li>
+    <li class="CMSListMenuLI"><a class="CMSListMenuLink<?php if ($menuIndex == 6) echo 'Highlighted' ?>" href="/rent-now.php">&gt; Rent Now</a></li>
 </ul>
