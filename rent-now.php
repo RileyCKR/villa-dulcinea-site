6c9920d4fa784c6e12e8dee7f384f265b63467a2
Wydra <RileyCKR@gmail.com>
2013-04-18 23:17:57 -0400

    add "Rent Now" page

---
+++ rent-now.php
@@ -0,0 +1,18 @@
+<?php 
+$title = 'Contact';
+$menuIndex = 6;
+$headerImage = '/images/header/dining-room.jpg';
+
+require '/layout/header.php'; ?>
+
+
+                        
+<h1>Rent Now</h1>
+<h2>Rental/General Inquiries</h2>
+<p>To obtain more information about the experience of your lifetime, 
+contact Geoffrey Boller.</p>
+
+<p style="padding-left: 16px;">Geoffrey Boller<br>
+<a href="mailto:VillaOnTheAegean@gmail.com">VillaOnTheAegean@gmail.com</a></p>
+
+<?php require '/layout/footer.php'; ?>
