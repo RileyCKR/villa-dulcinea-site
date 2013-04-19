4f3b0f2324be09154591fec1d67cd67a5b191568
Wydra <RileyCKR@gmail.com>
2013-04-18 23:02:35 -0400

    Add 'Area' page

---
+++ area.php
@@ -0,0 +1,60 @@
+<?php 
+$title = 'Area';
+$menuIndex = 3;
+$headerImage = '/images/header/outside-pool.jpg';
+
+require '/layout/header.php'; ?>
+
+
+                        
+<div class="leftColumn">
+    <h1>Area</h1>
+    <p>Turkbuku offers fabulous restaurants, nightclubs, and beaches as well 
+    as a variety of water sports.</p>
+    
+    <p>The Turkbuku-Bodrum area offers daily scuba diving excursions with 
+    reefs and wrecks to choose from.</p>
+
+    <p>Bodrum and its historic castle, ancient theatre, local shopping and 
+    famous nightlife are within a half hour drive.&nbsp; Seaside dining in 
+    neighboring Yalikavak, and sailing on the Aegean are also on offer.</p>
+
+    <p>Day trips to cultural sites in Izmir and Ephesus can easily be 
+    arranged.</p>
+    
+    <p>In a romantic mood? You may prefer dancing on the beaches in Turkbuku 
+    under the moonlight or star-gazing from the balcony.</p>
+</div>
+<div class="rightColumn">
+    <h2>Articles</h2>
+    <ul>
+        <li>
+            <a target="_blank" href="/getattachment/a37f4c08-8fea-4fc2-9e76-0498f37aa3f3/Bodrum-NYTimes.aspx">
+                <img src="/images/pdficon_large.gif" alt="pdf" border="0"> 
+                New York Times Article
+            </a>
+        </li>
+        <li>
+            <a target="_blank" href="/getattachment/296d7e31-868c-4aac-b0c4-149b85a9d5dc/Bodrum-Newsweek.aspx">
+                <img src="/images/pdficon_large.gif" alt="pdf" border="0">
+                Newsweek Article
+            </a>
+        </li>
+        <li>
+            <a target="_blank" href="/getattachment/7f2fe8c9-4a7d-47c9-8594-c53665185163/Bodrum-FinancialTimes.aspx">
+                <img src="/images/pdficon_large.gif" alt="pdf" border="0">
+                Financial Times Article
+            </a>
+        </li>
+    </ul>
+
+    <h2>Cultural Links</h2>
+    <ul>
+        <li><a href="http://www.bodrum.org/">Bodrum information, pictures, attractions</a></li>
+        <li><a href="http://www.allaboutturkey.com/bodrum.htm">All about Turkey - Bodrum</a></li>
+    </ul>
+</div>
+
+                    
+
+<?php require '/layout/footer.php'; ?>
